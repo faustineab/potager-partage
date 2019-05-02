c01285cf85ad669de8e55003424160e9d4686834
V-i-C-o <vincent.costiou01@gmailcom>
2019-05-02 19:14:59 +0200

    create CreateGarden and JoinGarden containers

---
+++ react/src/containers/CreateGarden.js
@@ -0,0 +1,13 @@
+import { connect } from 'redux';
+
+import CreateGarden from 'src/components/Subscribe/createGarden';
+
+const mapStateToProps = '';
+
+const mapDispatchToProps = {};
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(CreateGarden);
