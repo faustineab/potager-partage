1274d7027d75779876b9eb832cf09f25321dd4cc
JulieMuru <julie.muru@yahoo.fr>
2019-05-03 14:51:10 +0200

    modifs à remodifier

--- symfo/src/Controller/SecurityController.php
+++ symfo/src/Controller/SecurityController.php
@@ -12,7 +12,7 @@ class SecurityController extends AbstractController
     /**
      * @Route("/login", name="app_login")
      */
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils)
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
