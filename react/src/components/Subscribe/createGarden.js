be111fd5de80739ceb7940c77e98e92ad580d52b
V-i-C-o <vincent.costiou01@gmailcom>
2019-05-07 10:02:35 +0200

    correct css issue with background-image coverage

--- react/src/components/Subscribe/createGarden.js
+++ react/src/components/Subscribe/createGarden.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button, Form } from 'semantic-ui-react';
 
-import './index.scss';
+// import './index.scss';
 
 
 const CreateGarden = ({
