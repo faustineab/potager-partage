896d2f1c40dbbe18d37f8ec29e9942673f7ae6b8
JulieMuru <julie.muru@yahoo.fr>
2019-05-08 15:47:05 +0200

    registration admin + jardin + role ok

--- symfo/src/Controller/RegistrationController.php
+++ symfo/src/Controller/RegistrationController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Entity\Garden;
-use App\Repository\GardenRepository;
 use App\Form\AdminType;
 use App\Form\RegistrationUserType;
+use App\Repository\RoleRepository;
+use App\Repository\UserRepository;
+use App\Repository\GardenRepository;
 use App\Security\LoginFormAuthenticator;
 use Symfony\Component\Validator\Validation;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,50 +16,138 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Validator\Constraints as Assert;
-use App\Repository\UserRepository;
 use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register/admin", name="app_register")
      */
-    public function registerAdmin(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
+    public function registerAdmin(Request $request, GardenRepository $gardenRepository, UserPasswordEncoderInterface $encoder, ObjectManager $manager, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator, RoleRepository $roleRepository): Response
     {
 
-        // encode the plain password
+        $content = $request->getContent();
+        dump($content);
 
-        $hashedPassword = $passwordEncoder->encodePassword($userGarden['user'], $userGarden['user']->getPassword());
-        $userGarden['user']->setPassword($hashedPassword);
-        //   $userGarden['user']->addRole('ROLE_ADMIN'); set rôle admin à faire 
+        $data = json_decode($request->getContent(), true);
+
+        $validator = Validation::createValidator();
+
+        $constraint = new Assert\Collection(array(
+            // the keys correspond to the keys in the input array
+            'name' => new Assert\Length(array('min' => 1)),
+            'password' => new Assert\Length(array('min' => 1)),
+            'email' => new Assert\Email(),
+            'phone' => new Assert\Length(array('min' => 1)),
+            'address' => new Assert\Length(array('min' => 1)),
+            'gardenName' => new Assert\Length(array('min' => 1)),
+            'gardenAddress' => new Assert\Length(array('min' => 1)),
+            'gardenZipCode' => new Assert\Length(array('min' => 1)),
+            'gardenCity' => new Assert\Length(array('min' => 1)),
+            'gardenSpecificities' => new Assert\Length(array('min' => 1)),
+            'gardenMeters' => new Assert\Length(array('min' => 1)),
+            'gardenPlots_Row' => new Assert\Length(array('min' => 1)),
+            'gardenPlots_Column' => new Assert\Length(array('min' => 1)),
+
+        ));
+
+        $violations = $validator->validate($data, $constraint);
+
+        if ($violations->count() > 0) {
+            return new JsonResponse(["error" => (string)$violations], 500);
+        }
+
+        $username = $data['name'];
+        $password = $data['password'];
+        $email = $data['email'];
+        $phone = $data['phone'];
+        $address = $data['address'];
+        $gardenName = $data['gardenName'];
+        $gardenAddress = $data['gardenAddress'];
+        $gardenZipCode = $data['gardenZipCode'];
+        $gardenCity = $data['gardenCity'];
+        $gardenSpecificities = $data['gardenSpecificities'];
+        $gardenMeters = $data['gardenMeters'];
+        $gardenPlots_Row = $data['gardenPlots_Row'];
+        $gardenPlots_Column = $data['gardenPlots_Column'];
 
 
-        $userGarden['user']->setStatut('ok');
+        $user = new User();
+        $hashedPassword = $encoder->encodePassword($user, $password);
 
-        $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->persist($userGarden['user']);
-        $entityManager->persist($userGarden['garden']);
-        $entityManager->flush();
+        $user->setStatut('ok')
+            ->setName($username)
+            ->setPassword($hashedPassword)
+            ->setEmail($email)
+            ->setPhone($phone)
+            ->setAddress($address);
 
-        // do anything else you need here, like send an email
+        $manager->persist($user);
+        dump($user);
 
-        return $guardHandler->authenticateUserAndHandleSuccess(
-            $userGarden['user'],
-            $request,
-            $authenticator,
-            'main' // firewall name in security.yaml
-        );
+        $garden = new Garden();
+        $garden->setName($gardenName)
+            ->setAddress($gardenAddress)
+            ->setZipCode($gardenZipCode)
+            ->setCity($gardenCity)
+            ->setAddressSpecificities($gardenSpecificities)
+            ->setMeters($gardenMeters)
+            ->setNumberPlotsRow($gardenPlots_Row)
+            ->setNumberPlotsColumn($gardenPlots_Column);
 
 
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
+        $manager->persist($garden);
+        $manager->flush();
+        dump($garden);
+
+
+        $garden = $gardenRepository->findOneBy([
+            'name' => $gardenName
         ]);
+        dump($garden);
+
+        $garden->addUser($user);
+        dump($garden);
+
+
+        $manager->persist($garden);
+        dump($garden);
+
+        $role = $roleRepository->findOneBy([
+            'label' => 'Administrateur'
+        ]);
+        $role->addUser($user);
+
+        $manager->persist($role);
+        $manager->flush();
+        dump($role);
+        exit;
+        // $garden->addUser($user);
+        // $manager->persist($garden);
+
+        // $manager->flush();
+
+        // dump($garden);
+        // exit;
+
+
+        $json = [
+            'username' => $data['email'],
+            'password' => $data['password']
+        ];
+        dump($json);
+        exit;
+
+
+
+        return $this->redirectToRoute('app_login', $json, 307);
     }
+
 
     /**
      * @Route("/register/user", name="registration", methods={"GET","POST"})
