a4e3f01c544d7a59f976c9a76da3ebab02cef908
sousou33 <sophia.gautier@gmail.com>
2019-05-09 09:40:42 +0200

    mise à jour julie

--- symfo/src/Controller/EventController.php
+++ symfo/src/Controller/EventController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\Event;
 use App\Form\EventType;
 use Symfony\Component\HttpFoundation\Request;
@@ -11,35 +9,30 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\EventRepository;
+
 
 class EventController extends AbstractController
 {
     /**
-     * @Route("/event", name="create_event", methods={"GET","POST"})
+     * @Route("/api/event", name="create_event", methods={"GET","POST"})
      */
-
-
     public function create(Request $request, ObjectManager $manager, ValidatorInterface $validator)
     {
-        dump($request);
         $content = $request->getContent();
-
         $event = $this->get('serializer')->deserialize($content, Event::class, 'json');
-
         $errors = $validator->validate($event);
-
         if (count($errors) > 0) {
             dd($errors);
         }
-
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $event->setUser($user);
         $manager->persist($event);
         $manager->flush();
-
         return $this->redirectToRoute('show_event', [
             'id' => $event->getId(),
         ], Response::HTTP_CREATED);
     }
-
     /**
      * @Route("/event/{id}", name="show_event", methods={"GET"})
      */
@@ -48,7 +41,48 @@ class EventController extends AbstractController
         $data = $this->get('serializer')->serialize($event, 'json', ['groups' => ['event']]);
         $response = new Response($data);
         $response->headers->set('Content-Type', 'application/json');
-
         return $response;
+    }
+    /**
+     * @Route("/event/{id}/edit", name="edit_event", methods={"GET"})
+     */
+    public function edit(Event $event, Request $request, ObjectManager $manager, ValidatorInterface $validator)
+    {
+        $data = $this->get('serializer')->serialize($event, 'json', ['groups' => ['event']]);
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
+    }
+    /**
+     * @Route("api/event/{id}/edit", name="edit_event_post", methods={"POST"})
+     */
+    public function edit_post(Event $event, Request $request, ObjectManager $manager, ValidatorInterface $validator, EventRepository $eventRepository)
+    {
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        if ($user == $event->getUser()) {
+            $content = $request->getContent();
+            $currentEvent = $this->get('serializer')->deserialize($content, Event::class, 'json');
+            dump($currentEvent);
+            $errors = $validator->validate($event);
+            if (count($errors) > 0) {
+                dd($errors);
+            }
+            $description = $currentEvent->getDescription();
+            $title = $currentEvent->getTitle();
+            $startDate = $currentEvent->getStartDate();
+            $endDate = $currentEvent->getEndDate();
+            $event->setDescription($description)
+                ->setTitle($title)
+                ->setStartDate($startDate)
+                ->setEndDate($endDate);
+            // $event->setUser($user);
+            $manager->persist($event);
+            $manager->flush();
+            return $this->redirectToRoute('show_event', [
+                'id' => $event->getId(),
+            ], Response::HTTP_CREATED);
+        } else {
+            echo 'non autorisé';
+        }
     }
 }
--- symfo/src/Controller/RegistrationController.php
+++ symfo/src/Controller/RegistrationController.php
@@ -1,12 +1,12 @@
 <?php
-
 namespace App\Controller;
-
 use App\Entity\User;
 use App\Entity\Garden;
-use App\Repository\GardenRepository;
 use App\Form\AdminType;
 use App\Form\RegistrationUserType;
+use App\Repository\RoleRepository;
+use App\Repository\UserRepository;
+use App\Repository\GardenRepository;
 use App\Security\LoginFormAuthenticator;
 use Symfony\Component\Validator\Validation;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,73 +14,23 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Validator\Constraints as Assert;
-
-
-
 class RegistrationController extends AbstractController
 {
     /**
      * @Route("/register/admin", name="app_register")
      */
-    public function registerAdmin(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $authenticator): Response
-    {
-        $userGarden = [
-            'user' => new User(),
-            'garden' => new Garden()
-        ];
-
-
-
-        $form = $this->createForm(AdminType::class, $userGarden);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-
-            $hashedPassword = $passwordEncoder->encodePassword($userGarden['user'], $userGarden['user']->getPassword());
-            $userGarden['user']->setPassword($hashedPassword);
-            //   $userGarden['user']->addRole('ROLE_ADMIN'); set rôle admin à faire 
-
-
-            $userGarden['user']->setStatut('ok');
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($userGarden['user']);
-            $entityManager->persist($userGarden['garden']);
-            $entityManager->flush();
-
-            // do anything else you need here, like send an email
-
-            return $guardHandler->authenticateUserAndHandleSuccess(
-                $userGarden['user'],
-                $request,
-                $authenticator,
-                'main' // firewall name in security.yaml
-            );
-        }
-
-        return $this->render('registration/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/register/user", name="registration", methods={"GET","POST"})
-     */
-    public function registerUser(GardenRepository $gardenRepository, Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder, User $user = null): Response
+    public function registerAdmin(Request $request, GardenRepository $gardenRepository, UserPasswordEncoderInterface $encoder, ObjectManager $manager, RoleRepository $roleRepository): Response
     {
         $content = $request->getContent();
         dump($content);
-
         $data = json_decode($request->getContent(), true);
-
         $validator = Validation::createValidator();
-
         $constraint = new Assert\Collection(array(
             // the keys correspond to the keys in the input array
             'name' => new Assert\Length(array('min' => 1)),
@@ -88,34 +38,124 @@ class RegistrationController extends AbstractController
             'email' => new Assert\Email(),
             'phone' => new Assert\Length(array('min' => 1)),
             'address' => new Assert\Length(array('min' => 1)),
+            'gardenName' => new Assert\Length(array('min' => 1)),
+            'gardenAddress' => new Assert\Length(array('min' => 1)),
+            'gardenZipCode' => new Assert\Length(array('min' => 1)),
+            'gardenCity' => new Assert\Length(array('min' => 1)),
+            'gardenSpecificities' => new Assert\Length(array('min' => 1)),
+            'gardenMeters' => new Assert\Length(array('min' => 1)),
+            'gardenPlots_Row' => new Assert\Length(array('min' => 1)),
+            'gardenPlots_Column' => new Assert\Length(array('min' => 1)),
         ));
-
         $violations = $validator->validate($data, $constraint);
-
         if ($violations->count() > 0) {
             return new JsonResponse(["error" => (string)$violations], 500);
         }
-
         $username = $data['name'];
         $password = $data['password'];
         $email = $data['email'];
         $phone = $data['phone'];
         $address = $data['address'];
-
-
+        $gardenName = $data['gardenName'];
+        $gardenAddress = $data['gardenAddress'];
+        $gardenZipCode = $data['gardenZipCode'];
+        $gardenCity = $data['gardenCity'];
+        $gardenSpecificities = $data['gardenSpecificities'];
+        $gardenMeters = $data['gardenMeters'];
+        $gardenPlots_Row = $data['gardenPlots_Row'];
+        $gardenPlots_Column = $data['gardenPlots_Column'];
         $user = new User();
-        $user->setStatut('à valider')
+        $hashedPassword = $encoder->encodePassword($user, $password);
+        $user->setStatut('ok')
             ->setName($username)
-            ->setPassword($password)
+            ->setPassword($hashedPassword)
             ->setEmail($email)
             ->setPhone($phone)
             ->setAddress($address);
-
-
         $manager->persist($user);
+        dump($user);
+        $garden = new Garden();
+        $garden->setName($gardenName)
+            ->setAddress($gardenAddress)
+            ->setZipCode($gardenZipCode)
+            ->setCity($gardenCity)
+            ->setAddressSpecificities($gardenSpecificities)
+            ->setMeters($gardenMeters)
+            ->setNumberPlotsRow($gardenPlots_Row)
+            ->setNumberPlotsColumn($gardenPlots_Column);
+        $manager->persist($garden);
         $manager->flush();
-
-
-        return new JsonResponse(["success" => $user->getUsername() . " has been registered!"], 200);
+        dump($garden);
+        $garden = $gardenRepository->findOneBy([
+            'name' => $gardenName
+        ]);
+        dump($garden);
+        $garden->addUser($user);
+        dump($garden);
+        $manager->persist($garden);
+        dump($garden);
+        $role = $roleRepository->findOneBy([
+            'label' => 'Administrateur'
+        ]);
+        $role->addUser($user);
+        $manager->persist($role);
+        $manager->flush();
+        dump($role);
+        $credentials = [
+            'username' => $data['email'],
+            'password' => $data['password']
+        ];
+        dump($credentials);
+        return  new JsonResponse($credentials);
+    }
+    /**
+     * @Route("/register/user", name="registration", methods={"GET","POST"})
+     *  @return JsonResponse|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function registerUser(GardenRepository $gardenRepository, Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder, User $user = null, UserRepository $userRepository): Response
+    {
+        $content = $request->getContent();
+        dump($content);
+        $data = json_decode($request->getContent(), true);
+        $validator = Validation::createValidator();
+        $constraint = new Assert\Collection(array(
+            // the keys correspond to the keys in the input array
+            'name' => new Assert\Length(array('min' => 1)),
+            'password' => new Assert\Length(array('min' => 1)),
+            'email' => new Assert\Email(),
+            'phone' => new Assert\Length(array('min' => 1)),
+            'address' => new Assert\Length(array('min' => 1)),
+            'gardenId' => new Assert\Length(array('min' => 1))
+        ));
+        $violations = $validator->validate($data, $constraint);
+        if ($violations->count() > 0) {
+            return new JsonResponse(["error" => (string)$violations], 500);
+        }
+        $username = $data['name'];
+        $password = $data['password'];
+        $email = $data['email'];
+        $phone = $data['phone'];
+        $address = $data['address'];
+        $gardenId = $data['gardenId'];
+        $user = new User();
+        $hashedPassword = $encoder->encodePassword($user, $password);
+        $user->setStatut('à valider')
+            ->setName($username)
+            ->setPassword($hashedPassword)
+            ->setEmail($email)
+            ->setPhone($phone)
+            ->setAddress($address);
+        $manager->persist($user);
+        $garden = $gardenRepository->find($gardenId);
+        $garden->addUser($user);
+        $manager->persist($garden);
+        $manager->flush();
+        dump($garden);
+        $credentials = [
+            'username' => $data['email'],
+            'password' => $data['password']
+        ];
+        dump($credentials);
+        return  new JsonResponse($credentials);
     }
 }
