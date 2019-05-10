e9ba4be9029f328a4e8a2b79cb5142d04742ef6d
JulieMuru <julie.muru@yahoo.fr>
2019-05-10 18:59:14 +0200

    validation ou non d'un user (statut à valider) ok

--- symfo/src/Controller/AdminController.php
+++ symfo/src/Controller/AdminController.php
@@ -7,11 +7,12 @@ use App\Entity\User;
 use App\Repository\RoleRepository;
 use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 
 class AdminController extends AbstractController
 {
@@ -48,31 +49,53 @@ class AdminController extends AbstractController
     }
 
     /**
-     * @Route("api/admin/status/validation", name="admin_status_validation", methods={"POST"})
+     * @Route("api/admin/status/user/{id}/validation", name="admin_status_validation", methods={"POST"})
      */
-    public function validation(UserRepository $userRepository, RoleRepository $roleRepository, Request $request)
+    public function validation(UserRepository $userRepository, RoleRepository $roleRepository, Request $request, ValidatorInterface $validator, ObjectManager $manager, User $user)
     {
+
+
         $currentUser = $this->get('security.token_storage')->getToken()->getUser();
+
 
         $role = $roleRepository->findBy(['label' => 'administrateur']);
         $currentUserRoles = $currentUser->getRoles();
 
 
         foreach ($role as $roleName) {
-            $user = $roleName->getName();
+            $userRole = $roleName->getName();
 
-            if (array_search($user, $currentUserRoles) !== false) {
+            if (array_search($userRole, $currentUserRoles) !== false) {
 
-                $statut = $request->getContent();
-                dump($statut);
-                $data = $this->get('serializer')->deserialize($statut, User::class, 'json');
-                dd($data);
+                $usersToBeAuthorized = $userRepository->findby(['statut' => 'à valider']);
+
+                if (array_search($user, $usersToBeAuthorized) !== false) {
+
+                    $content = $request->getContent();
+                    dump($content);
+                    $data = $this->get('serializer')->deserialize($content, User::class, 'json');
+                    dump($data);
+
+                    $errors = $validator->validate($data);
+
+                    if (count($errors) > 0) {
+                        dd($errors);
+                    }
+
+                    $statut = $data->getStatut();
+
+                    $user->setStatut($statut);
 
 
-                return $response;
-            } else {
+                    $manager->persist($user);
+                    $manager->flush();
+
+                    return new JsonResponse("le statut à bien été modifié", 200);
+                } else {
+                    return new JsonResponse("Votre statut à déjà été confirmé", 500);
+                }
+            } else
                 return new JsonResponse("Vous n' êtes pas autorisé à accéder à cette page", 500);
-            }
         }
     }
 }
