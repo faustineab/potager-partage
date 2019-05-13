96a6c01dd639897cd5e94ff28eeb6e3bfdb4dfa2
JulieMuru <julie.muru@yahoo.fr>
2019-05-13 15:40:42 +0200

    test

--- symfo/src/Controller/SecurityController.php
+++ symfo/src/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
