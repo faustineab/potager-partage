90e85876b8ab22e315a684aab75e6741f846393c
V-i-C-o <vincent.costiou01@gmailcom>
2019-05-14 09:54:31 +0200

    change base URL for axios requests

--- react/src/store/ajaxMiddleware.js
+++ react/src/store/ajaxMiddleware.js
@@ -12,7 +12,7 @@ const ajaxMiddleware = store => next => (action) => {
   switch (action.type) {
     case FETCH_GARDENLIST:
       next(action);
-      axios.get('http://localhost/Projet/potager-partage/symfo/public/register/user')
+      axios.get('http://localhost/apo/potager-partage/symfo/public/register/user')
         .then((response) => {
           // console.log('response data', response.data);
           const gardenList = response.data.map(garden => ({
@@ -29,7 +29,7 @@ const ajaxMiddleware = store => next => (action) => {
 
     case CREATE_GARDEN:
       next(action);
-      axios.post('http://localhost/Projet/potager-partage/symfo/public/register/admin', {
+      axios.post('http://localhost/apo/potager-partage/symfo/public/register/admin', {
         name: `${store.getState().firstName} ${store.getState().lastName}`,
         email: store.getState().email,
         password: store.getState().password,
@@ -54,7 +54,7 @@ const ajaxMiddleware = store => next => (action) => {
 
     case JOIN_GARDEN:
       next(action);
-      axios.post('http://localhost/Projet/potager-partage/symfo/public/register/user', {
+      axios.post('http://localhost/apo/potager-partage/symfo/public/register/user', {
         name: `${store.getState().firstName} ${store.getState().lastName}`,
         gardenId: store.getState().gardenId,
         email: store.getState().email,
@@ -74,7 +74,7 @@ const ajaxMiddleware = store => next => (action) => {
       break;
     case LOG_USER:
       next(action);
-      axios.post('http://localhost/Projet/potager-partage/symfo/public/login', {
+      axios.post('http://localhost/apo/potager-partage/symfo/public/login', {
         username: store.getState().email,
         password: store.getState().password,
       })
